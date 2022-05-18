56d721328c572b29d7a5aec28c536bb60671c45f
FabioQua2003 <fabioqudrelli@gmail.com>
2022-05-18 10:00:20 +0200

    Implemento codice

---
+++ Articoli.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AS2122_4E_INF_QuadrelliFabio_GestioneArticoli
+{
+    class Articolo 
+    {
+        string codice;
+        string descrzione;
+        string unitadimisura;
+        double prezzo;
+
+        public Articolo(string codice, string descrzione, string unitadimisura, double prezzo)
+            {
+                this.codice = codice;
+                this.descrzione = descrzione;
+                this.unitadimisura = unitadimisura;
+                this.prezzo = prezzo;
+        }
+    
+    public string Codice { get { return codice; } };
+    public string Descrizione { get { return descrizione; } }
+    public string UnitaDiMisura { get { return unitadimisura; } }
+    public double Prezzo { get { return prezzo; } }
+    }
+}
